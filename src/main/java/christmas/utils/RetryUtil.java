0b157a917ee697602e78b43838fab0793950f5e2
donghyunkim <ghffu405@gmail.com>
2023-12-14 00:01:32 +0900

    Feat: 입력값 받는 유틸 메소드 추가

---
+++ src/main/java/christmas/utils/RetryUtil.java
@@ -0,0 +1,16 @@
+package christmas.utils;
+
+import java.util.function.Supplier;
+
+public class RetryUtil {
+
+    public static <T> T getInput(Supplier<T> inputSupplier){
+        while(true){
+            try{
+                return inputSupplier.get();
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
