422ae5b7f05aa3097a35863070ae040fb94c208f
donghyunkim <ghffu405@gmail.com>
2023-12-14 00:00:18 +0900

    feat: 리드미 작성 및 초기 클래스 생성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.controller.EventController;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        EventController eventController = new EventController(new InputView(), new OutputView());
+        eventController.run();
     }
 }
---
+++ src/main/java/christmas/controller/EventController.java
@@ -0,0 +1,19 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class EventController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public EventController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run(){
+
+    }
+}
