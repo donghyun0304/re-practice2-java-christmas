9cedef75a93bab98f57958c0a8316c15e07fe0a7
donghyunkim <ghffu405@gmail.com>
2023-12-14 00:13:42 +0900

    feat: EventDate클래스 생성

---
+++ src/main/java/christmas/domain/EventDate.java
@@ -0,0 +1,28 @@
+package christmas.domain;
+
+import christmas.utils.ErrorMessage;
+
+import javax.swing.*;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+public class EventDate {
+
+    private static final int YEAR = 2023;
+    private static final int MONTH = 12;
+
+    private final LocalDate date;
+
+    private EventDate(final LocalDate date) {
+        this.date = date;
+    }
+
+    public static EventDate from(int day){
+        try {
+            LocalDate.of(YEAR, MONTH, day);
+        } catch (DateTimeException e){
+            throw new IllegalArgumentException(ErrorMessage.NOT_VALID_DATE, e);
+        }
+        return new EventDate(LocalDate.of(YEAR, MONTH, day));
+    }
+}
--- src/main/java/christmas/utils/ErrorMessage.java
+++ src/main/java/christmas/utils/ErrorMessage.java
@@ -4,5 +4,7 @@ public class ErrorMessage {
 
     public static final String NOT_NUMERIC = "[ERROR] 정수가 아닙니다. ";
     public static final String NOT_RANGE = "[ERROR] 범위안에 존재하지 않습니다.";
+    public static final String NOT_VALID_DATE = "[ERROR] 날짜가 유효하지 않습니다.";
+
 
 }
